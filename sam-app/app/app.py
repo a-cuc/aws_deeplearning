f34cddc63ebfe7419674aada297c3f685ed5c3c6
a-cuc <qalccu@tip.edu.ph>
2026-07-04 09:28:15 +0800

    Refactoring lambda function to reflect torch inference

--- sam-app/app/app.py
+++ sam-app/app/app.py
@@ -1,28 +1,48 @@
 import joblib
-import base64
-import numpy as np
+import torch
+from torch import nn
 import json
 
-from io import BytesIO
-from PIL import Image
 
-model_file = '/opt/ml/model'
-model = joblib.load(model_file)
+device = "cpu"
+
+# Defining the neural network architecture
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTMModel, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size, device=x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size, device=x.device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+model = LSTMModel(input_size=2, hidden_size=24, num_layers=2, output_size=1).to(device)
+
+# Loading the trained model and scaler
+model.load_state_dict(torch.load('/opt/ml/model/lstm_model.pth'))
+scaler = joblib.load('/opt/ml/model/scaler.pkl')
+
+""" Preprocessing input data for prediction
+data_scaled = scaler.transform(raw_data)  # Example input data
+
+"""
 
 
-def lambda_handler(event, context):
-    image_bytes = event['body'].encode('utf-8')
-    image = Image.open(BytesIO(base64.b64decode(image_bytes))).convert(mode='L')
-    image = image.resize((28, 28))
 
-    x = np.array(image)
-    prediction = int(model.predict(x.reshape(1, -1))[0])
+
+def lambda_handler(event, context): 
+    # TODO: Consider input for preprocessing
 
     return {
         'statusCode': 200,
         'body': json.dumps(
             {
-                "predicted_label": prediction,
+                "predicted_result": prediction,
             }
         )
     }
